ce694cc1ab51fad737793b49cbdd92fe90a735c5
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2021-03-17 16:38:52 +0100

    Initial commit

---
+++ app/src/main/java/org/cerbrendus/garduino/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.cerbrendus.garduino
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.cerbrendus.garduino.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
